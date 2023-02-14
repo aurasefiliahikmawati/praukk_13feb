ca4220141c136e5976ae70061428680fbfa43628
aurasefiliahikmawati <109121021+aurasefiliahikmawati@users.noreply.github.com>
2023-02-14 11:57:17 +0700

    praukk_13feb
    praukk_13feb

---
+++ praukk_aura/db_praukk_auraa.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Feb 2023 pada 05.54
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_praukk_auraa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_bahan`
+--
+
+CREATE TABLE `tb_bahan` (
+  `id_bahan` int(11) NOT NULL,
+  `nama_bahan` varchar(50) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_bahan`
+--
+
+INSERT INTO `tb_bahan` (`id_bahan`, `nama_bahan`, `satuan`, `harga`) VALUES
+(1, 'sepatu', '6', 30000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_toko`
+--
+
+CREATE TABLE `tb_toko` (
+  `id_toko` int(11) NOT NULL,
+  `nama_toko` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_siup` int(11) NOT NULL,
+  `nama_pemilik` varchar(50) NOT NULL,
+  `id_bahan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_toko`
+--
+
+INSERT INTO `tb_toko` (`id_toko`, `nama_toko`, `alamat`, `no_siup`, `nama_pemilik`, `id_bahan`) VALUES
+(1, 'barokah', 'prampelan', 6, 'aku', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_bahan`
+--
+ALTER TABLE `tb_bahan`
+  ADD PRIMARY KEY (`id_bahan`);
+
+--
+-- Indeks untuk tabel `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  ADD PRIMARY KEY (`id_toko`),
+  ADD KEY `id_bahan` (`id_bahan`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_bahan`
+--
+ALTER TABLE `tb_bahan`
+  MODIFY `id_bahan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  ADD CONSTRAINT `tb_toko_ibfk_1` FOREIGN KEY (`id_bahan`) REFERENCES `tb_bahan` (`id_bahan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
